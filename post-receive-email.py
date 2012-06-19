cda16e6056c1f9f14b1f95dc2b3858ad5ed062ec
Ignacio Casal Quinteiro <icq@gnome.org>
2012-06-19 17:58:36 +0200

    Add myself to copyright

--- post-receive-email.py
+++ post-receive-email.py
@@ -4,6 +4,7 @@
 #
 # Copyright (C) 2008  Owen Taylor
 # Copyright (C) 2009  Red Hat, Inc
+# Copyright (C) 2012  Ignacio Casal Quinteiro
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
